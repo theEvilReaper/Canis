371e506c57b1c5f24e8015b6ee6b80c9372bd87c
theEvilReaper <steffenwx@gmail.com>
2022-11-03 12:06:36 +0100

    Add NotNull annotation and mark translation method as static

--- src/main/java/net/theevilreaper/canis/util/DoorFacing.java
+++ src/main/java/net/theevilreaper/canis/util/DoorFacing.java
@@ -1,6 +1,7 @@
 package net.theevilreaper.canis.util;
 
 import net.minestom.server.instance.block.BlockFace;
+import org.jetbrains.annotations.NotNull;
 
 public enum DoorFacing {
 
@@ -9,12 +10,19 @@ public enum DoorFacing {
     SOUTH,
     WEST;
 
-    public DoorFacing translate(BlockFace blockFace) {
+    /**
+     * Translates a given {@link BlockFace} to a DoorFacing entry.
+     * @param blockFace the face to translate
+     * @return
+     */
+    //TODO: Check what the client does when the BlockFace is top
+    public static DoorFacing translate(@NotNull BlockFace blockFace) {
         return switch (blockFace) {
             case EAST -> DoorFacing.EAST;
+            case NORTH ->  DoorFacing.NORTH;
             case SOUTH -> DoorFacing.SOUTH;
             case WEST -> DoorFacing.WEST;
-            default -> DoorFacing.NORTH;
+            default -> throw new IllegalArgumentException("The given face can't be converted");
         };
     }
 }
