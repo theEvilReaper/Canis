1dd9ef36d8e5af1cf4f4cb081eaf957d06ed8305
theEvilReaper <steffenwx@gmail.com>
2022-11-17 22:42:47 +0100

    Add Constants class

---
+++ src/main/java/net/theevilreaper/canis/util/Constants.java
@@ -0,0 +1,14 @@
+package net.theevilreaper.canis.util;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since
+ **/
+
+public class Constants {
+
+    public static final int MAX_BUILD_HEIGHT = 255;
+
+    private Constants() {}
+}
