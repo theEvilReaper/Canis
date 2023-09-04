ef60b0b961287a8b438bf05017f4251a73519dbf
theEvilReaper <steffenwx@gmail.com>
2023-09-04 10:07:38 +0200

    Add dependencyResolutionManagement section which contains all dependencies for the project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "Canis"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("minestom", "net.onelitefeather.microtus", "Minestom").version("1.1.0")
+            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("1.1.0")
+            library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").version("5.10.0")
+            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.0")
+        }
+    }
+}
