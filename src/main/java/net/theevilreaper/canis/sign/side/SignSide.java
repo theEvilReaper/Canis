5b184d418395b3bee450fa6a73e57451eae4008c
theEvilReaper <steffenwx@gmail.com>
2023-11-10 21:41:30 +0100

    Add SignSide enum

---
+++ src/main/java/net/theevilreaper/canis/sign/side/SignSide.java
@@ -0,0 +1,15 @@
+package net.theevilreaper.canis.sign.side;
+
+/**
+ * Enum representing the sides of a sign where text can be placed.
+ * With Mojang's integration of text on both sides of a sign, this enum provides
+ * identifiers for the available sides where text can be shown.
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+public enum SignSide {
+
+    FRONT,
+    BACK
+}
