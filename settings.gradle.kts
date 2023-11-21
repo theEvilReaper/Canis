d0e7239d11c3c4591f4479d24fea168895852211
theEvilReaper <steffenwx@gmail.com>
2023-11-21 16:05:27 +0100

    Update microtus version to 1.1.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,12 @@ rootProject.name = "Canis"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("minestom", "net.onelitefeather.microtus", "Minestom").version("1.1.0")
-            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("1.1.0")
-            library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").version("5.10.0")
-            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.0")
+            version("microtus", "1.1.1")
+            version("junit", "5.10.0")
+            library("minestom", "net.onelitefeather.microtus", "Minestom").versionRef("microtus")
+            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("microtus")
+            library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").versionRef("junit")
+            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
         }
     }
 }
