fa1374232e52adebfe1ee6bd73e65cfbbf53ff76
theEvilReaper <steffenwx@gmail.com>
2023-11-21 21:37:55 +0100

    Fix wrong version usage for the test dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
             version("microtus", "1.1.1")
             version("junit", "5.10.0")
             library("minestom", "net.onelitefeather.microtus", "Minestom").versionRef("microtus")
-            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("microtus")
+            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").versionRef("microtus")
             library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").versionRef("junit")
             library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
         }
