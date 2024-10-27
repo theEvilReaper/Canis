fe12d5a11a9e18f323334ce9216cdc19abeb7e20
theEvilReaper <steffenwx@gmail.com>
2024-10-27 20:09:15 +0100

    Bump Microtus version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "Canis"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("microtus", "1.4.2")
+            version("microtus", "1.5.0-SNAPSHOT")
             version("junit", "5.11.3")
             library("minestom", "net.onelitefeather.microtus", "Microtus").versionRef("microtus")
             library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("microtus")
