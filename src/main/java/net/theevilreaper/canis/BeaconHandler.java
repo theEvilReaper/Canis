26739a32fd094a23f7abebb0e6fafaf66004ce6d
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-14 18:18:10 +0200

    Migrate handler implementation to the updated structure

--- src/main/java/net/theevilreaper/canis/BeaconHandler.java
+++ src/main/java/net/theevilreaper/canis/BeaconHandler.java
@@ -1,8 +1,8 @@
 package net.theevilreaper.canis;
 
+import net.kyori.adventure.key.Key;
 import net.minestom.server.instance.block.BlockHandler;
 import net.minestom.server.tag.Tag;
-import net.minestom.server.utils.NamespaceID;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -10,13 +10,14 @@ import java.util.List;
 
 /**
  * Implementation of a {@link BlockHandler} which reads the nbt tags for a beacon.
+ *
  * @author theEvilReaper
- * @version 1.0.0
+ * @version 1.1.0
  * @since 1.0.0
  **/
 public class BeaconHandler implements BlockHandler {
 
-    private static final NamespaceID BEACON_TAG = NamespaceID.from("minecraft:beacon");
+    private static final Key BEACON_TAG = Key.key("minecraft:beacon");
 
     private static final List<Tag<?>> BEACON_TAGS = List.of(
             Tag.String("CustomName"),
@@ -27,7 +28,8 @@ public class BeaconHandler implements BlockHandler {
     );
 
     /**
-     * Returns all tags which are needed for the block.
+     * Returns all tags that are needed for the block.
+     *
      * @return the list with the tags
      */
     @Override
@@ -35,12 +37,8 @@ public class BeaconHandler implements BlockHandler {
         return BEACON_TAGS;
     }
 
-    /**
-     * Returns the {@link NamespaceID} for the block.
-     * @return the given namespace
-     */
     @Override
-    public @NotNull NamespaceID getNamespaceId() {
+    public @NotNull Key getKey() {
         return BEACON_TAG;
     }
 }
--- src/main/java/net/theevilreaper/canis/SkullHandler.java
+++ src/main/java/net/theevilreaper/canis/SkullHandler.java
@@ -3,7 +3,6 @@ package net.theevilreaper.canis;
 import net.kyori.adventure.key.Key;
 import net.minestom.server.instance.block.BlockHandler;
 import net.minestom.server.tag.Tag;
-import net.minestom.server.utils.NamespaceID;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
@@ -11,13 +10,14 @@ import java.util.List;
 
 /**
  * Implementation of a {@link BlockHandler} which reads the nbt tags for a skull.
+ *
  * @author theEvilReaper
- * @version 1.0.0
+ * @version 1.1.0
  * @since 1.0.0
  **/
 public class SkullHandler implements BlockHandler {
 
-    private static final NamespaceID namespaceID = NamespaceID.from(Key.key("minecraft:skull"));
+    private static final Key SKULL_KEY = Key.key("minecraft:skull");
 
     private static final List<Tag<?>> TAG_LIST = List.of(
             Tag.String("custom_name"),
@@ -26,20 +26,17 @@ public class SkullHandler implements BlockHandler {
     );
 
     /**
-     * Returns all tags which are needed for the block.
+     * Returns all tags that are needed for the block.
+     *
      * @return the list with the tags
      */
     @Override
     public @NotNull Collection<Tag<?>> getBlockEntityTags() {
-       return TAG_LIST;
+        return TAG_LIST;
     }
 
-    /**
-     * Returns the {@link NamespaceID} for the block.
-     * @return the given namespace
-     */
     @Override
-    public @NotNull NamespaceID getNamespaceId() {
-        return namespaceID;
+    public @NotNull Key getKey() {
+        return SKULL_KEY;
     }
 }
